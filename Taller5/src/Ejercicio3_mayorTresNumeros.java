564bb5e834eab973ec0079861c309b6b12183888
Sebitxss <jscarrion15@utpl.edu.ec>
2024-11-07 12:35:17 -0500

    ejercicio3

--- Taller5/src/Ejercicio3_mayorTresNumeros.java
+++ Taller5/src/Ejercicio3_mayorTresNumeros.java
@@ -7,14 +7,32 @@ import java.util.Scanner;
  */
 public class Ejercicio3_mayorTresNumeros {
     public static void main(String[] args) {
-        int a, b, c, mayor;
+        int a, b, c;
         Scanner sca = new Scanner (System.in);
         System.out.println("Ingrese 3 números: ");
         a = sca.nextInt();
         b = sca.nextInt();
         c = sca.nextInt();
-        mayor = (a>b) || (b>c) ? a : b : c;
+               if (a == b && b == c) {
+            System.out.println("Los tres números son iguales.");
+        } else {
+            int mayor;
+            if (a > b && a > c) {
+                mayor = a;
+            } else if (b > a && b > c) {
+                mayor = b;
+            } else {
+                mayor = c;
+            }
 
+            System.out.println("El mayor es: " + mayor);
+        }
     }
     
 }
+/***
+ * Ingrese 3 números: 
+2 7 4
+El mayor es: 7
+BUILD SUCCESSFUL (total time: 12 seconds)
+ */
