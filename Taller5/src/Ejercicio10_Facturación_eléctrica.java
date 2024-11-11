2346505cc80ee266579aac167fee6416725f3732
Julian-2606 <julianfjs1982@gmail.com>
2024-11-10 19:34:21 -0500

    ejercicio10

---
+++ Taller5/src/Ejercicio10_Facturación_eléctrica.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Ejercicio10_Facturación_eléctrica {
+
+    public static void main(String[] args) {
+        double vpluz, costokxh, kconsumidos, edad_u, valorbase, descuento;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("INGRESE EL VALOR DEL COSTO DE KILOVATIO POR HORA: ");
+        costokxh = teclado.nextDouble();
+        System.out.print("INGRESE EL NUMERO DE KILOVATIOS CONSUMIDOS: ");
+        kconsumidos = teclado.nextDouble();
+        System.out.print("INGRESE LA EDAD DEL USUARIO: ");
+        edad_u = teclado.nextDouble();
+        valorbase = costokxh * kconsumidos;
+        if (edad_u > 65) {
+            valorbase = (costokxh * kconsumidos) * (0.9);
+            vpluz = valorbase;
+            System.out.println("EL VALOR A PAGAR DE LA PLANILLA DE LUZ ES: $ " + vpluz);
+        } else {
+            vpluz = valorbase;
+            System.out.println("EL VALOR A PAGAR DE LA PLANILLA DE LUZ ES: $ " + valorbase);
+        }
+    }
+}
--- Taller5/src/Ejercicio8_CalculadoraDeIMC.java
+++ Taller5/src/Ejercicio8_CalculadoraDeIMC.java
@@ -1,27 +1,42 @@
+
 import java.util.Scanner;
+
 public class Ejercicio8_CalculadoraDeIMC {
+
     public static void main(String[] args) {
-        double peso_kg, altura, peso;
+        double pesokg, altura, peso;
         Scanner tcl = new Scanner(System.in);
         System.out.print("INGRESE SU PESO EN KILOGRAMOS: ");
-        peso_kg = tcl.nextDouble();
+        pesokg = tcl.nextDouble();
         System.out.print("INGRESE SU ESTATURA: ");
         altura = tcl.nextDouble();
-        peso = (peso_kg / (altura * altura));
-        if (peso > 0 && peso <= 18)
+        peso = (pesokg / (altura * altura));
+        if (peso > 0 && peso <= 18) {
             System.out.println("EL IMC DE LA PERSONA SE ENCUENTRA EN BAJO PESO");
-        else
-            if (peso > 18.5 && peso <= 25)
-                System.out.println("EL IMC DE LA PERSONA SE ENCUENTRA EN UN PESO NORMAL");
-        if (peso > 25 && peso <= 30)
+        } else if (peso > 18.5 && peso <= 25) {
+            System.out.println("EL IMC DE LA PERSONA SE ENCUENTRA EN UN PESO NORMAL");
+        }
+        if (peso > 25 && peso <= 30) {
             System.out.println("EL IMC DE LA PERSONA SE ENCUENTRA EN SOBREPESO");
-        if (peso > 30 && peso <= 35)
+        }
+        if (peso > 30 && peso <= 35) {
             System.out.println("EL IMC DE LA PERSONA SE ENCUENTRA EN OBESIDAD LEVE");
-        if (peso > 35 && peso <= 40)
+        }
+        if (peso > 35 && peso <= 40) {
             System.out.println("EL IMC DE LA PERSONA SE ENCUENTRA EN OBESIDAD MODERADA");
-        if (peso > 40 && peso <= 100)
+        }
+        if (peso > 40 && peso <= 100) {
             System.out.println("EL IMC DE LA PERSONA SE ENCUENTRA EN OBESIDAD MORBIDA");
-        
+        }
+
     }
-    
+
 }
+
+/***
+ * run-single:
+INGRESE SU PESO EN KILOGRAMOS: 70
+INGRESE SU ESTATURA: 175
+EL IMC DE LA PERSONA SE ENCUENTRA EN BAJO PESO
+BUILD SUCCESSFUL (total time: 8 seconds)
+ */
